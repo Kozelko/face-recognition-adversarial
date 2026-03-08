9300b3788fe59fb5eae4af95cac2a73a25dbe501
Peter Brandajský <xbrandajsky@stuba.sk>
2026-03-08 22:45:07 +0100

    feat: add ConvBlock module

---
+++ models/benchmark_cnn.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.act1 = nn.PReLU(out_channels)
+
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.act2 = nn.PReLU(out_channels)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.act1(self.bn1(self.conv1(x)))
+        x = self.act2(self.bn2(self.conv2(x)))
+        return self.pool(x)
